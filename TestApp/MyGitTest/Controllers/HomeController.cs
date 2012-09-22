7cbb5249453d2437dc4bbfd7de6de2cfa11303e7
Yogish <yogishm@gmail.com>
2012-09-22 14:10:14 +0530

    Test Commit from Ext

--- TestApp/MyGitTest/Controllers/HomeController.cs
+++ TestApp/MyGitTest/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MyGitTest.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC! Edited from http";
+            ViewBag.Message = "Welcome to ASP.NET MVC! Edited from VS";
 
             return View();
         }
