d84de7d5c06a7ec3cee5fbfcb742799c943dca17
yogishm <yogishm@gmail.com>
2012-09-22 14:13:16 +0530

    from Git now

--- TestApp/MyGitTest/Controllers/HomeController.cs
+++ TestApp/MyGitTest/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MyGitTest.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC! Edited from VS";
+            ViewBag.Message = "Welcome to ASP.NET MVC! Edited from Http again";
 
             return View();
         }
